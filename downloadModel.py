5a85e481386b934e2005390148fd85268bef650b
MooNknightO2 <wsygg97286@gmail.com>
2026-01-10 19:18:58 +0800

    feat: add download

--- downloadModel.py
+++ downloadModel.py
@@ -2,6 +2,7 @@ from huggingface_hub import snapshot_download
 
 dir1 = "./models/pythia-70m"
 dir2 = "./models/pythia-2.8b"
+dir3 = "./models/pythia-410m"
 print("下载70M模型")
 snapshot_download(
     repo_id="EleutherAI/pythia-70m",
@@ -14,4 +15,11 @@ snapshot_download(
     local_dir=dir2,
     local_dir_use_symlinks=False,
 )
+print("下载410m模型")
+snapshot_download(
+    repo_id="EleutherAI/pythia-410m",
+    local_dir=dir3,
+    local_dir_use_symlinks=False,
+)
+
 print(f"模型下载完成")
